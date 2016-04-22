37460a787dd3b014f0fe262aec05dbc3e7594b76
Ubuntu <ubuntu@ip-172-31-32-240.us-west-2.compute.internal>
2016-04-22 22:46:11 +0000

    modified production.py

--- mysite/production.py
+++ mysite/production.py
@@ -2,5 +2,5 @@ from settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'ec2-52-39-21-223.us-west-2.compute.amazonaws.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
